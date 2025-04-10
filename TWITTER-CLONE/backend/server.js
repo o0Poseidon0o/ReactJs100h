4ca23016fd042dd2e1ee0f07d9377ac6af38fc27
o0Poseidon0o <90851953+o0Poseidon0o@users.noreply.github.com>
2025-04-10 07:33:04 +0700

    dang nhap
    ReacjJS

--- TWITTER-CLONE/backend/server.js
+++ TWITTER-CLONE/backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
 import dotenv from "dotenv";
+
+import cookieParser from "cookie-parser"
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
 
@@ -8,9 +11,13 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json()); //allows us to accept JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); //to parse form data (urlendcode)
+
+
+app.use(cookieParser());
 
 // console.log(process.env.MONGO_URI);
 
